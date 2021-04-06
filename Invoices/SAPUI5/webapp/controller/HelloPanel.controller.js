2fc105a4fef516af7ee66b1d01486fb15a33ab64
xavier92ca <franciscox92@outlook.com>
2021-04-06 16:42:12 +0000

    .

--- Invoices/SAPUI5/webapp/controller/HelloPanel.controller.js
+++ Invoices/SAPUI5/webapp/controller/HelloPanel.controller.js
@@ -1,16 +1,18 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/m//MessageToast",
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.m.MessageToast} MessageToast
      */
-    function (Controller) {
+    function (Controller, MessageToast) {
 
         "use strict";
 
 
-        return Controller.extend("logaligroup.SAPUI5.controller.App", {
+        return Controller.extend("logaligroup.SAPUI5.controller.HelloPanel", {
 
             onInit: function () {
 
